9d4910a86912d31544beeac16be6c00a7f1b9991
Anton Stroka <astroka@perfsys.com>
2025-03-04 18:31:33 +0100

    deelte

--- app/api/auth/[slug]/route.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthRouteHandlers } from "@/utils/data-server-utils";
-
-export const GET = createAuthRouteHandlers({
-  redirectOnSignInComplete: "/home",
-  redirectOnSignOutComplete: "/sign-in",
-});
